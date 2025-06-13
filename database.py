94f5759a0304ca347f5f42836de43e35642299c1
Cristian <suruceanu.cristian@gmail.com>
2025-06-13 04:53:12 +0300

    Better database environment variables approach

--- database.py
+++ database.py
@@ -7,9 +7,12 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()  # loads variables from .env into environment
 
-database_url = os.getenv("DATABASE_URL")
+database_username = os.getenv("DATABASE_USERNAME")
+database_password = os.getenv("DATABASE_PASSWORD")
+database_name = os.getenv("DATABASE_NAME")
+database_host = os.getenv("DATABASE_HOST")
 
-engine = create_engine(database_url)
+engine = create_engine(f"postgresql://${database_username}:${database_password}@${database_host}:5432/${database_name}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
