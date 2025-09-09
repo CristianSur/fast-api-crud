2346e60d1e5538ac85100f918574bdaac5ed07d8
Cristian <suruceanu.cristian@gmail.com>
2025-09-09 15:55:49 +0300

    refactoring and update requests.http

--- repository.py
+++ repository.py
@@ -1,23 +1,32 @@
+from typing import Optional
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-import schemas
+from schemas import *
 from models import Product
+from schemas import ProductRead
 
 
-def get_by_id(db: Session, product_id: int):
+def get_by_id(db: Session, product_id: int) -> Optional[ProductRead]:
     stmt = select(Product).where(Product.id == product_id)
-    return db.execute(stmt).scalar_one_or_none()
+    product = db.execute(stmt).scalar_one_or_none()
+
+    if product is None:
+        return None
+
+    return ProductRead.model_validate(product)
 
 
-def get_all(db: Session):
+def get_all(db: Session) -> list[ProductRead]:
     stmt = select(Product)
-    return db.execute(stmt).scalars().all()
+    products = db.execute(stmt).scalars().all()
+    return [ProductRead.model_validate(p) for p in products]
 
 
-def create_product(db: Session, product: schemas.ProductCreate):
+def create_product(db: Session, product: ProductCreate) -> ProductRead:
     db_product = Product(**product.model_dump())
     db.add(db_product)
     db.commit()
     db.refresh(db_product)
-    return schemas.ProductRead.model_validate(db_product)
+    return ProductRead.model_validate(db_product)
